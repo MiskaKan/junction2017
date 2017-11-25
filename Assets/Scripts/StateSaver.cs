27b4f81455500b0f63d8c53941f045afd502108d
Olli Piuhola <command@master.local>
2017-11-25 16:47:23 +0200

    Added StateSaver initial class

---
+++ Assets/Scripts/StateSaver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class StateSaver {
+
+	public int maxLevel = 1;
+
+	public static void Save () {
+		BinaryFormatter bf = new BinaryFormatter();
+		Debug.Log (Application.persistentDataPath + "/levelSave.pole");
+		PlayerData data = new PlayerData ();
+		data.maxLevel = maxLevel;
+		FileStream file = File.Create (Application.persistentDataPath + "/levelSave.pole");
+		bf.Serialize (file, data);
+		file.Close ();
+	}
+
+	public static void Load() {
+		if (File.Exists (Application.persistentDataPath + "/levelSave.pole")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open(Application.persistentDataPath + "/levelSave.pole", FileMode.Open);
+			StateSaver.maxLevel = (PlayerData)bf.Deserialize (file);
+			file.Close ();
+		}
+	}
+}
+
+[Serializable]
+class PlayerData {
+	public int maxLevel;
+}
