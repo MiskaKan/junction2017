74331292ec2780d4384e06ecededf2d709414281
Olli Piuhola <command@master.local>
2017-11-25 11:05:47 +0200

    Add listeners to levelbuttons

---
+++ Assets/Scripts/LevelButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButtons : MonoBehaviour {
+	public Button leftButton;
+	public Button rightButton;
+	// Use this for initialization
+	void Start () {
+		leftButton.onClick.AddListener(PreviousLevelPress);
+		rightButton.onClick.AddListener(NextLevelPress);
+	}
+
+	void PreviousLevelPress() {
+		Debug.Log ("Prev. pressed");
+	}
+
+	void NextLevelPress() {
+		Debug.Log ("Next pressed");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
