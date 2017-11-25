c4432e40831100cb0c85958c370db1d98b792f9f
Karri Lehtiranta <karri.lehtiranta@aalto.fi>
2017-11-25 10:22:35 +0200

    Add simple GameManager

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{                  
+    public static Main instance = null;
+    private LevelManager levelScript;                       
+    private int level = 1;                                  
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        levelScript = GetComponent<LevelManager>();
+        InitGame();
+    }
+
+    void OnLevelWasLoaded(int index) {
+        level = index;
+        InitGame();
+    }
+
+    void InitGame() {
+        levelScript.SetupScene(level);
+    }
+
+    void Update() {
+    }
+
+    public void GameOver() {
+        enabled = false;
+    }
 }
