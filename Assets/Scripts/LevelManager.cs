63e3481726eee09244928703863c99e4baf456d9
Karri Lehtiranta <karri.lehtiranta@aalto.fi>
2017-11-25 15:19:36 +0200

    Added more levels

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -17,10 +17,39 @@ public class LevelManager : MonoBehaviour {
         DestoryClones();
         levels = new List<GameObject>() { level1, level2, level3, level4, level5 };
         if (level > -1 && level < levels.Count) {
-            GameObject clone = Instantiate(levels[level], new Vector3(0, 0, 0), Quaternion.Euler(90,0,180));
-            clone.gameObject.tag = "clone";
-        }
-        
+            GameObject clone;
+            if (level == 0) {
+                clone = Instantiate(levels[level], new Vector3(0, 0, 0), Quaternion.Euler(90, 0, 180));
+                clone.gameObject.transform.localScale = new Vector3(0.2f, 0, 0.2f);
+                clone.gameObject.transform.localPosition = new Vector3(0.8f, 4.1f, 0);
+                clone.gameObject.tag = "clone";
+                clone.SetActive(true);
+            } else if (level == 1) {
+                clone = Instantiate(levels[level], new Vector3(0, 0, 0), Quaternion.Euler(180, 0, 180));
+                clone.gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                clone.gameObject.transform.localPosition = new Vector3(5.0f, 3.9f, 0);
+                clone.gameObject.tag = "clone";
+                clone.SetActive(true);
+            } else if (level == 2) {
+                clone = Instantiate(levels[level], new Vector3(0, 0, 0), Quaternion.Euler(0, 180, 0));
+                clone.gameObject.transform.localScale = new Vector3(4f, 4f, 4f);
+                clone.gameObject.transform.localPosition = new Vector3(2.5f, 4.8f, 0);
+                clone.gameObject.tag = "clone";
+                clone.SetActive(true);
+            } else if (level == 3) {
+                clone = Instantiate(levels[level], new Vector3(0, 0, 0), Quaternion.Euler(180, 0, 180));
+                clone.gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                clone.gameObject.transform.localPosition = new Vector3(5.0f, 4.8f, 0);
+                clone.gameObject.tag = "clone";
+                clone.SetActive(true);
+            } else if (level == 4) {
+                clone = Instantiate(levels[level], new Vector3(0, 0, 0), Quaternion.Euler(180, 0, 180));
+                clone.gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                clone.gameObject.transform.localPosition = new Vector3(5.0f, 4f, 0);
+                clone.gameObject.tag = "clone";
+                clone.SetActive(true);
+            }
+        }    
     }
 
     public void DestoryClones() {
